d2c029bbf1d9b09d8c94ff48fdb1cfb605a75c09
Haroon <haroonchelsea2761@gmail.com>
2021-03-25 03:06:58 +0500

    Added assets & navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,62 +1,180 @@
 <template>
   <div>
     <Nuxt />
+    <!-- Sign In Popup
+================================================== -->
+    <div id="sign-in-dialog" class="zoom-anim-dialog mfp-hide dialog-with-tabs">
+      <!--Tabs -->
+      <div class="sign-in-form">
+        <ul class="popup-tabs-nav">
+          <li><a href="#login">Log In</a></li>
+          <li><a href="#register">Register</a></li>
+        </ul>
+
+        <div class="popup-tabs-container">
+          <!-- Login -->
+          <div class="popup-tab-content" id="login">
+            <!-- Welcome Text -->
+            <div class="welcome-text">
+              <h3>We're glad to see you again!</h3>
+              <span
+                >Don't have an account?
+                <a href="#" class="register-tab">Sign Up!</a></span
+              >
+            </div>
+
+            <!-- Form -->
+            <form method="post" id="login-form">
+              <div class="input-with-icon-left">
+                <i class="icon-material-baseline-mail-outline"></i>
+                <input
+                  type="text"
+                  class="input-text with-border"
+                  name="emailaddress"
+                  id="emailaddress"
+                  placeholder="Email Address"
+                  required
+                />
+              </div>
+
+              <div class="input-with-icon-left">
+                <i class="icon-material-outline-lock"></i>
+                <input
+                  type="password"
+                  class="input-text with-border"
+                  name="password"
+                  id="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <a href="#" class="forgot-password">Forgot Password?</a>
+            </form>
+
+            <!-- Button -->
+            <button
+              class="button full-width button-sliding-icon ripple-effect"
+              type="submit"
+              form="login-form"
+            >
+              Log In <i class="icon-material-outline-arrow-right-alt"></i>
+            </button>
+
+            <!-- Social Login -->
+            <div class="social-login-separator"><span>or</span></div>
+            <div class="social-login-buttons">
+              <button class="facebook-login ripple-effect">
+                <i class="icon-brand-facebook-f"></i> Log In via Facebook
+              </button>
+              <button class="google-login ripple-effect">
+                <i class="icon-brand-google-plus-g"></i> Log In via Google+
+              </button>
+            </div>
+          </div>
+
+          <!-- Register -->
+          <div class="popup-tab-content" id="register">
+            <!-- Welcome Text -->
+            <div class="welcome-text">
+              <h3>Let's create your account!</h3>
+            </div>
+
+            <!-- Account Type -->
+            <div class="account-type">
+              <div>
+                <input
+                  type="radio"
+                  name="account-type-radio"
+                  id="freelancer-radio"
+                  class="account-type-radio"
+                  checked
+                />
+                <label for="freelancer-radio" class="ripple-effect-dark"
+                  ><i class="icon-material-outline-account-circle"></i>
+                  Freelancer</label
+                >
+              </div>
+
+              <div>
+                <input
+                  type="radio"
+                  name="account-type-radio"
+                  id="employer-radio"
+                  class="account-type-radio"
+                />
+                <label for="employer-radio" class="ripple-effect-dark"
+                  ><i class="icon-material-outline-business-center"></i>
+                  Employer</label
+                >
+              </div>
+            </div>
+
+            <!-- Form -->
+            <form method="post" id="register-account-form">
+              <div class="input-with-icon-left">
+                <i class="icon-material-baseline-mail-outline"></i>
+                <input
+                  type="text"
+                  class="input-text with-border"
+                  name="emailaddress-register"
+                  id="emailaddress-register"
+                  placeholder="Email Address"
+                  required
+                />
+              </div>
+
+              <div
+                class="input-with-icon-left"
+                title="Should be at least 8 characters long"
+                data-tippy-placement="bottom"
+              >
+                <i class="icon-material-outline-lock"></i>
+                <input
+                  type="password"
+                  class="input-text with-border"
+                  name="password-register"
+                  id="password-register"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+
+              <div class="input-with-icon-left">
+                <i class="icon-material-outline-lock"></i>
+                <input
+                  type="password"
+                  class="input-text with-border"
+                  name="password-repeat-register"
+                  id="password-repeat-register"
+                  placeholder="Repeat Password"
+                  required
+                />
+              </div>
+            </form>
+
+            <!-- Button -->
+            <button
+              class="margin-top-10 button full-width button-sliding-icon ripple-effect"
+              type="submit"
+              form="register-account-form"
+            >
+              Register <i class="icon-material-outline-arrow-right-alt"></i>
+            </button>
+
+            <!-- Social Login -->
+            <div class="social-login-separator"><span>or</span></div>
+            <div class="social-login-buttons">
+              <button class="facebook-login ripple-effect">
+                <i class="icon-brand-facebook-f"></i> Register via Facebook
+              </button>
+              <button class="google-login ripple-effect">
+                <i class="icon-brand-google-plus-g"></i> Register via Google+
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Sign In Popup / End -->
   </div>
 </template>
-
-<style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bookyourhours',
+    title: 'Hireo',
     htmlAttrs: {
       lang: 'en'
     },
@@ -11,7 +11,63 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/style.css' },
+      { rel: 'stylesheet', href: '/css/colors/blue.css' }
+    ],
+    script: [
+      {
+        src: "/js/jquery-3.5.1.min.js",
+        body: true,
+      },
+      {
+        src: "/js/jquery-migrate-3.3.1.min.js",
+        body: true,
+      },
+      {
+        src: "/js/mmenu.min.js",
+        body: true,
+      },
+      {
+        src: "/js/tippy.all.min.js",
+        body: true,
+      },
+      {
+        src: "/js/simplebar.min.js",
+        body: true,
+      },
+      {
+        src: "/js/bootstrap-slider.min.js",
+        body: true,
+      },
+      {
+        src: "/js/bootstrap-select.min.js",
+        body: true,
+      },
+      {
+        src: "/js/snackbar.js",
+        body: true,
+      },
+      {
+        src: "/js/clipboard.min.js",
+        body: true,
+      },
+      {
+        src: "/js/counterup.min.js",
+        body: true,
+      },
+      {
+        src: "/js/magnific-popup.min.js",
+        body: true,
+      },
+      {
+        src: "/js/slick.min.js",
+        body: true,
+      },
+      {
+        src: "/js/custom.js",
+        body: true,
+      },
     ]
   },
 
